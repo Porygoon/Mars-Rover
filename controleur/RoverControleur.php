2132f45b3e3ef4eb1d4ce8940a69c9a320b8afe9
jb <kurokncjb@gmail.com>
2016-11-18 11:22:31 +0100

    Correction du bug #2 - closes #2
    Correction du bug #2

--- controleur/RoverControleur.php
+++ controleur/RoverControleur.php
@@ -4,13 +4,21 @@ require_once '../modele/MapModele.php';
 require_once '../modele/ObstacleModele.php';
 
 function run() {
+    function randWithoutNumber ($excpetedNumber, $min = null, $max = null) {
+        $number = rand($min, $max);
+        while (in_array($excpetedNumber, array($number))) {
+            $number = rand($min, $max);
+        }
+        return $number;
+    }
+
     $X = isset($_POST['X']) ? $_POST['X'] : 5;
     $Y = isset($_POST['Y']) ? $_POST['Y'] : 5;
     $direction = isset($_POST['directionRover']) ? $_POST['directionRover'] : 'N';
-    $YObstacle = isset($_POST['YObstacleSave']) ? $_POST['YObstacleSave'] : rand(1, 9);
-    $XObstacle = isset($_POST['XObstacleSave']) ? $_POST['XObstacleSave'] : rand(1, 9);
-    $XAlien = isset($_POST['XAlienSave']) ? $_POST['XAlienSave'] : rand(1, 9);
-    $YAlien = isset($_POST['YAlienSave']) ? $_POST['YAlienSave'] : rand(1, 9);
+    $YObstacle = isset($_POST['YObstacleSave']) ? $_POST['YObstacleSave'] : randWithoutNumber(5, 1, 9);
+    $XObstacle = isset($_POST['XObstacleSave']) ? $_POST['XObstacleSave'] : randWithoutNumber(5, 1, 9);
+    $XAlien = isset($_POST['XAlienSave']) ? $_POST['XAlienSave'] : randWithoutNumber(5, 1, 9);
+    $YAlien = isset($_POST['YAlienSave']) ? $_POST['YAlienSave'] : randWithoutNumber(5, 1, 9);
     $scoreSave = isset($_POST['scoreSave']) ? $_POST['scoreSave'] : 0;
 
     $positionRover = ['X' => $X, 'Y' => $Y];
